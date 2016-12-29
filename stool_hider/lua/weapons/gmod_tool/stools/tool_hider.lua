f51d7f67c70f67bb88868d5c758d182612e36073
Joshua Willis <j.willis8@students.clark.edu>
2016-12-29 03:15:46 -0800

    Update tool_hider.lua

--- stool_hider/lua/weapons/gmod_tool/stools/tool_hider.lua
+++ stool_hider/lua/weapons/gmod_tool/stools/tool_hider.lua
@@ -8,21 +8,35 @@ TOOL.ClientConVar = {
 }
 
 // Initialization Stuff //
-if CLIENT then
-	hidden_entities = { }
-	
-	
-end
-
-if SERVER then
-	hidden_entities = { }
-	
-end
+hidden_entities = hidden_entities or { }
 
 // Tool Functionality Stuff //
 
+function contains( tbl, trg )
+	for k, v in pairs( tbl ) do
+		if v == trg then return true end
+	end
+	return false
+end
+
+if CLIENT then
+	function printHidden( )
+		for k, v in pairs( hidden_entities ) do
+			print( k.." : "..v:GetModel() )
+		end
+	end
+
+	concommand.Add( "tool_hider_printhidden", function( )
+		for k, v in pairs( hidden_entities ) do
+			print( k.." : "..v:GetModel() )
+		end
+	end )
+end
+
 if SERVER then
-	function TOOL:SetEntityHidden( ent )
+	function TOOL:SetEntityHidden( ent, ply )
+		if contains( hidden_entities, ent ) then return end
+		table.insert( hidden_entities, ent )
 		
 	end
 end
@@ -30,11 +44,22 @@ end
 // Tool Handling Stuff //
 function TOOL:LeftClick( trace )
 	if !trace.Entity then return false end
-
+	if trace.Entity:IsWorld() then return false end
 	if CLIENT then
-		
+		print( trace.Entity )
+		return true
 	end
 	
+	if !IsValid( trace.Entity ) then
+		return false
+	elseif !util.IsValidPhysicsObject( trace.Entity, trace.PhysicsBone ) then
+		return false
+	end
+	
+	local ent = trace.Entity
+	local ply = self:GetOwner()
+
+	self:SetEntityHidden( ent, ply )
 end
 
 function TOOL:RightClick( trace )
